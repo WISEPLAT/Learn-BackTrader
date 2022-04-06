0b14c8c9cf064e6f4a5f11f96fe981d7210d4a54
Oleg Shpagin <info@wiseplat.org>
2022-04-06 07:14:28 +0300

    +strategy

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ from __future__ import (absolute_import, division, print_function,
 import backtrader as bt
 import pandas as pd
 import datetime  # For datetime objects
-# import os.path  # To manage paths
-# import sys  # To find out the script name (in argv[0])
+import os.path  # To manage paths
+import sys  # To find out the script name (in argv[0])
 
 
-# Create a Strategy
+# Create a Stratey
 class TestStrategy(bt.Strategy):
 
     def log(self, txt, dt=None):
-        ''' Logging function for this strategy'''
+        ''' Logging function fot this strategy'''
         dt = dt or self.datas[0].datetime.date(0)
         print('%s, %s' % (dt.isoformat(), txt))
 
@@ -23,14 +23,82 @@ class TestStrategy(bt.Strategy):
         # Keep a reference to the "close" line in the data[0] dataseries
         self.dataclose = self.datas[0].close
 
+        # To keep track of pending orders
+        self.order = None
+
+    def notify_order(self, order):
+        if order.status in [order.Submitted, order.Accepted]:
+            # Buy/Sell order submitted/accepted to/by broker - Nothing to do
+            return
+
+        # Check if an order has been completed
+        # Attention: broker could reject order if not enough cash
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                self.log('BUY EXECUTED, %.2f' % order.executed.price)
+            elif order.issell():
+                self.log('SELL EXECUTED, %.2f' % order.executed.price)
+
+            self.bar_executed = len(self)
+
+        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
+            self.log('Order Canceled/Margin/Rejected')
+
+        # Write down: no pending order
+        self.order = None
+
+    def notify_trade(self, trade):
+        if not trade.isclosed:
+            return
+
+        self.log('OPERATION PROFIT, GROSS %.2f, NET %.2f' %
+                 (trade.pnl, trade.pnlcomm))
+
     def next(self):
         # Simply log the closing price of the series from the reference
         self.log('Close, %.2f' % self.dataclose[0])
+        # print("*", self.datas[0].close[0])
+
+        # Check if an order is pending ... if yes, we cannot send a 2nd one
+        if self.order:
+            return
+
+        # Check if we are in the market
+        if not self.position:
+
+            # Not yet ... we MIGHT BUY if ...
+            if self.dataclose[0] < self.dataclose[-1]:
+                    # current close less than previous close
+
+                    if self.dataclose[-1] < self.dataclose[-2]:
+                        # previous close less than the previous close
+
+                        # BUY, BUY, BUY!!! (with default parameters)
+                        self.log('BUY CREATE, %.2f' % self.dataclose[0])
+
+                        # Keep track of the created order to avoid a 2nd order
+                        self.order = self.buy()
+
+        else:
+
+            # Already in the market ... we might sell
+            if len(self) >= (self.bar_executed + 50):
+                # SELL, SELL, SELL!!! (with all possible default parameters)
+                self.log('SELL CREATE, %.2f' % self.dataclose[0])
+
+                # Keep track of the created order to avoid a 2nd order
+                self.order = self.sell()
 
 
 if __name__ == '__main__':
 
-    data = pd.read_csv('GAZP_D1.csv', sep=',', index_col='Date')    # this data is from metatrader 5
+    modpath = os.path.dirname(os.path.abspath(sys.argv[0]))
+    datapath = os.path.join(modpath, 'GAZP_D1.csv')
+
+    # print(datapath)
+    # exit(1)
+
+    data = pd.read_csv(datapath, sep=',', index_col='Date')    # this data is from metatrader 5
     print(data)
     data = data.reset_index()
     data.rename(columns={'Date': 'datetime', 'Open': 'open', 'High': 'high',
@@ -39,9 +107,6 @@ if __name__ == '__main__':
     data.index = pd.to_datetime(data['datetime'])
     print(data)
 
-    # modpath = os.path.dirname(os.path.abspath(sys.argv[0]))
-    # datapath = os.path.join(modpath, 'GAZP.ME (1).csv')
-
     cerebro = bt.Cerebro()
 
     # Add a strategy
@@ -49,9 +114,14 @@ if __name__ == '__main__':
 
     cerebro.broker.setcash(100000.0)
 
+    # Set the commission - 0.1% ... divide by 100 to remove the %
+    cerebro.broker.setcommission(commission=0.001)
+
+    # modpath = os.path.dirname(os.path.abspath(sys.argv[0]))
+    # datapath = os.path.join(modpath, 'GAZP.ME (1).csv')
     # Create a Data Feed
     # data = bt.feeds.YahooFinanceCSVData(
-    #     dataname='GAZP.ME (1).csv', # yahoo data downloaded from site
+    #     dataname=datapath, # yahoo data downloaded from site
     #     # Do not pass values before this date
     #     fromdate=datetime.datetime(2022, 1, 1),
     #     # Do not pass values after this date
